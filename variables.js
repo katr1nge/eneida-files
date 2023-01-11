3bbb0672e0bf45c15359f73700bbb81170818743
Katr1nge <38489257+katr1nge@users.noreply.github.com>
2023-01-11 13:25:57 +0200

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,7 @@
+var Header = "Відкриття! Та ще й великий заголовок";
+var element = document.getElementById("header");
+element.innerHTML = Header;
+
+var SubHead = "Стань частиною початку кінця російських серверів в Україні";
+var element2 = document.getElementById("sub-head");
+element2.innerHTML = SubHead;
