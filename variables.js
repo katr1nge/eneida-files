f0050ae7d24e399f77d5a6f4ec823779baea9bb8
Katr1nge <38489257+katr1nge@users.noreply.github.com>
2023-01-11 13:42:19 +0200

    Add files via upload

--- variables.js
+++ variables.js
@@ -5,3 +5,42 @@ element.innerHTML = Header;
 var SubHead = "Стань частиною початку кінця російських серверів в Україні";
 var element2 = document.getElementById("sub-head");
 element2.innerHTML = SubHead;
+
+var premiumdesc = `
+<ul>
+<li class="donp">Можливість робити <span class="doncolor">дабл-джамп</span> в лоббі</li>
+<li class="donp">Можливість встановити <span class="doncolor">скін за URL</span></li>
+<li class="donp">За вбивство на Smasher вам буде додаватися <span class="doncolor">2 монети</span></li>
+<li class="donp">Множник гришок <span class="doncolor">х1.5</span></li>
+<li class="donp">В чат буде надсилатися повідомлення, що <span class="doncolor">ви зайшли на режим</span></li>
+<li class="donp">Вам надається доступ до чату донатерів - <span class="doncolor">/dc</span> <br><span class="dongray">А також можливість його сховати - <span class="doncolor">/dctoggle</span></span></li>
+<li class="donp">Вам стають доступні для придбання нові ґаджети:</li>
+<ul>
+    <li class="sub"><span class="doncolor">Анімовані головні убори</span></li>
+    <li class="sub"><span class="doncolor">Емоції</span></li>
+</ul>
+</ul>
+<div class="text-center bbo"><a href="#"><button class="btn btn-primary">Придбати</button></a></div>
+`;
+var element3 = document.getElementById("premiumdesc");
+element3.innerHTML = premiumdesc;
+
+var deluxedesc = `
+<ul>
+<li class="donp"><span class="doncolor">усі можливості привілеї <span class="bbo">Premium</span></span></li>
+<li class="donp">Можливість <span class="doncolor">літати</span> в лоббі</li>
+<li class="donp">Можливість зайти на <span class="doncolor">заповнену BedWars арену</span></li>
+<li class="donp">Множник гришок <span class="doncolor">х2.0</span></li>
+<li class="donp">За вбивство на Smasher вам буде додаватися <span class="doncolor">3 монетки</span></li>
+<li class="donp">Вам стають доступні для придбання нові ґаджети:
+</li>
+<ul>
+  <li class="sub"><span class="doncolor">Плащі</span></li>
+  <li class="sub"><span class="doncolor">Банери</span></li>
+  <li class="sub"><span class="doncolor">Пети</span></li>
+</ul>
+</ul>
+<div class="text-center bbo"><a href="#"><button class="btn btn-primary">Придбати</button></a></div>
+`;
+var element4 = document.getElementById("deluxedesc");
+element4.innerHTML = deluxedesc;
