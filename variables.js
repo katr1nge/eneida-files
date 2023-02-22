8a1746e828ff35b53fa4f9f0197b7611664d316b
Katr1nge <38489257+katr1nge@users.noreply.github.com>
2023-02-22 17:21:57 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,4 +1,12 @@
 
+var Header = "Відкриття! Та ще й великий заголовок";
+var element = document.getElementById("header");
+element.innerHTML = Header;
+
+var SubHead = "Стань частиною початку кінця російських серверів в Україні";
+var element2 = document.getElementById("sub-head");
+element2.innerHTML = SubHead;
+
 
 var premiumdesc = `
 <ul>
